0b841a8103477cc664fbf7b15f68ee2915c2b396
blackhogwash <blackhogwash@gmail.com>
2018-09-23 21:41:59 +0300

    Changed file bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class BotHandler:
 
         if len(get_result) > 0:
             last_update = get_result[-1]
-        else:
+        elif get_result:
             last_update = get_result[len(get_result)]
 
         return last_update
